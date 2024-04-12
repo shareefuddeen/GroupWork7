16f8e6a7e1d4a5f545393946025ce2aecb466973
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2024-04-12 08:40:08 -1200

    vercel

--- Sports/Sports/settings.py
+++ Sports/Sports/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,14 +64,16 @@ WSGI_APPLICATION = 'Sports.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'oYPijOHDOKrjuGNIBtwoHLBiZyRpuaNS',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '21993',
     }
 }
-
 
 
 # Password validation
@@ -110,9 +112,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
